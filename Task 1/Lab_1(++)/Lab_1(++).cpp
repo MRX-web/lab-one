92568f80661265b70c6768601e0e1e2472706c62
Kirill <57215654+MRX-web@users.noreply.github.com>
2020-12-19 14:29:57 +0300

    Add files via upload

---
+++ Task 1/Lab_1(++)/Lab_1(++).cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+/*
+Объявить массив из n=20 целых чисел, проинициализировать нулями. Функция
+process_array() должна заполнить массив случайными числами от 1 до 10, вычислить
+и вернуть наиболее часто встречающееся значение в массиве (если таких несколько,
+вернуть наименьшее) и сформировать выходной массив из всех элементов, которые
+встречаются как минимум 2 раза во входном массиве. Вывести на экран массивы.
+
+*/
+void printArray(int* arr, size_t size)//Функция вывода массива 
+{
+	for (int i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+const size_t N = 20;//Задаем размер массива
+
+int processArray(int* a, int* b, size_t n) {
+	int count = 0;
+	int maxcount = 0;
+	int number = 0;
+	//перебираем все элементы массива
+	for (int i = 0; i < N; i++)
+	{
+		count = 0;
+		//перебираем все элементы от i до конца
+		for (int j = i; j < N; j++) {
+			if (a[i] == a[j])
+				count++;
+		}
+
+		if (maxcount == count) {
+			if (number > a[i]) {
+				number = a[i];
+			}
+		}
+		if (maxcount < count) {
+			number = a[i];
+			maxcount = count;
+		}
+	}
+	for (int i = 0; i < N; i++) {
+		b[a[i] - 1]++;
+	}
+	return number;
+}
+
+int main()
+{
+	int a[N] = { 2, 10, 3, 5, 8, 7, 3, 3, 7, 10, 5, 2, 4, 5, 4, 3, 2, 2, 5, 1 };//Создаем входной массив из N элементов
+	int b[10] = { 0 };//Создаем выходной массив из 10 элементов
+	processArray(a, b, 20);
+	printf("\n");
+	printArray(a, N);
+	printf("\n");
+	for (int i = 0; i < 10; i++) {
+		if (b[i] >= 2)
+			printf("%d ", i + 1);
+	}
+	return 0;
+	//srand(0);//Инициализируем рандомайзер 
+}
---
+++ Task 2/CPP_Tsk_2/CPP_Tsk_2.cpp
@@ -0,0 +1,59 @@
+﻿// Lab_1(++)(Task_2).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+/*
+Преобразование: 2D → 1D. Двумерный массив 5х3 вещественных чисел необходимо
+выложить в один ряд по элементам слева направо и снизу вверх.
+Инициализация: заполнить массив числами x[i][j] = sin(i–j) + cos(i+j).
+Вывод на экран: на каждый элемент массива отвести 14 позиций.
+*/
+
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include<Math.h>
+
+
+#ifdef _DEBUG
+#include <crtdbg.h>
+#define _CRTDBG_MAP_ALLOC
+#endif
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	// Создаем двумерный массив и память под него
+	int n = 5;
+	int m = 3;
+	double** array = new double* [n];
+	for (int i = 0; i < n; i++)
+		array[i] = new double[m];
+	cout << "Array:\n";
+	for (int i = n - 1; i >= 0; i--)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			array[i][j] = (sin(i - j) + cos(i + j));
+			//array[i][j] = 0;
+			cout << setw(4) << array[i][j];
+		}
+		cout << endl;
+	}
+
+	// переводим его в одномерный
+	double * arr_1d = new double[n * m];
+	int k = 0;
+	cout << endl << "1D Array: ";
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			arr_1d[k] = array[i][j];
+			cout << arr_1d[k] << " ";
+			k++;
+		}
+		delete[] array[i];
+	}
+	delete[] array;
+	delete[] arr_1d;
+	return 0;
+
+}
